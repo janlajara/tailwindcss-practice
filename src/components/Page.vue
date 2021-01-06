29037fee5c9159ca7a3bf8839dd5cd53afe0e401
Jan Lajara <janlajara@gmail.com>
2021-01-06 18:02:58 +0800

    ui components Page and Section

---
+++ src/components/Page.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="mx-8 py-4 md:mx-16 md:py-8">
+        <h1>{{$props.title}}</h1>
+        <slot/>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String
+    }
+}
+</script>
---
+++ src/components/Section.vue
@@ -0,0 +1,18 @@
+<template>
+    <article class="my-4">
+        <div class="my-2">
+            <h2 class="text-lg">{{$props.heading}}</h2>
+            <p class="text-gray-600">{{$props.description}}</p>
+        </div>
+        <slot/>
+    </article>
+</template>
+
+<script>
+export default {
+    props: {
+        heading: String,
+        description: String
+    }
+}
+</script>
