74e986e3dd2cd60c9872e63dc826b07dad5b834b
Jan Lajara <janlajara@gmail.com>
2021-01-07 17:53:49 +0800

    added buttons with icon

--- src/views/pages/Buttons.vue
+++ src/views/pages/Buttons.vue
@@ -1,14 +1,29 @@
 <template>
     <div>
         <Page title="Buttons">
-            <Section heading="Examples of Buttons"
-                description="Here are the samples of buttons in various styles."
+            <Section heading="Regular Buttons"
+                description="Here are the samples of regular buttons in various styles."
                 class="sm:flex">
                 <div id="buttons-group" class="flex flex-grow flex-wrap justify-center my-auto">
                     <Button type="primary">Primary</Button>
                     <Button type="primary">Orange Button</Button>
                     <Button type="secondary">Secondary</Button>
                     <Button type="secondary">White Button</Button>
+                </div>
+            </Section>
+            <Section heading="Buttons with Icon"
+                description="Here are the samples of buttons with icon in various styles."
+                class="sm:flex">
+                <div id="buttons-group" class="flex flex-grow flex-wrap justify-center my-auto">
+                    <Button type="primary">
+                        Kagat!<template v-slot:icon><Icon id="drumstick-bite" class="w-6"/></template>
+                    </Button>
+                    <Button type="secondary">
+                        Download<template v-slot:icon><Icon id="download" class="w-6"/></template>
+                    </Button>
+                    <Button type="secondary">
+                        Delete<template v-slot:icon><Icon id="times" class="w-6"/></template>
+                    </Button>
                 </div>
             </Section>
         </Page>
@@ -20,9 +35,14 @@ import Page from '@/components/Page.vue'
 import Section from '@/components/Section.vue'
 import Button from '@/components/Button.vue'
 
+import Icon from '@/components/Icon.vue'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faDrumstickBite, faDownload, faTimes} from '@fortawesome/free-solid-svg-icons'
+library.add(faDrumstickBite, faDownload, faTimes)
+
 export default {
     components: {
-        Page, Section, Button
+        Page, Section, Button, Icon
     }
 }
 </script>
