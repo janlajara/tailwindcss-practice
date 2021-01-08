fb1db8f55a469f07df5d0b6550a96c0ac7548a9c
Jan Lajara <janlajara@gmail.com>
2021-01-08 16:03:51 +0800

    removed bg color

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="bg-gray-100 py-8 px-8 md:px-16 text-center md:text-left">
+    <footer class="py-8 px-8 md:px-16 text-center md:text-left">
         <p class="text-sm text-gray-500">Copyright © Shereps 2021</p>
     </footer>
 </template>
