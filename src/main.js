9bac477b5d68bb8127b5207ad5f4d094bdb996ee
Jan Lajara <janlajara@gmail.com>
2021-01-17 16:53:14 +0800

    toast notifications

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+import './assets/toasts.css'
 
 createApp(App)
     .use(router)
+    .use(Toast, {
+        hideProgressBar: true,
+        position: 'bottom-right'
+    })
     .mount('#app')
--- src/views/pages/Notifications.vue
+++ src/views/pages/Notifications.vue
@@ -1,14 +1,45 @@
 <template>
     <Page title="Notifications">
+        <Section heading="Toasts" description="Click any of the buttons to trigger the toasts."
+            class="flex">
+            <div class="buttons-group flex flex-grow flex-wrap justify-center my-auto">
+                <Button type="primary" :action="()=>showToast()">Default</Button>
+                <Button type="secondary" :action="()=>showToast('success')">Success</Button>
+                <Button type="secondary" :action="()=>showToast('info')">Info</Button>
+                <Button type="secondary" :action="()=>showToast('warning')">Warning</Button>
+                <Button type="secondary" :action="()=>showToast('error')">Error</Button>
+            </div>
+        </Section>
+        <Section heading="Modals" description="Click any of the buttons to trigger the modals.">
+
+        </Section>
     </Page>
 </template>
 
 <script>
 import Page from '@/components/Page.vue'
+import Section from '@/components/Section.vue'
+import Button from '@/components/Button.vue'
+import {useToast} from 'vue-toastification'
 
 export default {
     components: {
-        Page
+        Page, Section, Button
+    },
+    setup(){
+        const toast = useToast()
+        const showToast = (type='default')=> {
+            return toast("Bulaga!", {type})
+        }
+        return {
+            showToast
+        }
     }
 }
 </script>
+
+<style scoped>
+.buttons-group button {
+    @apply m-2;
+}
+</style>
