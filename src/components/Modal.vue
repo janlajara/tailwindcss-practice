2286303fc4e3c4d0db7b97f7f1574aaa604a3ead
Jan Lajara <janlajara@gmail.com>
2021-01-18 18:04:29 +0800

    modals

---
+++ src/components/Modal.vue
@@ -0,0 +1,36 @@
+<template>
+    <teleport to="body">
+        <div class="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-30">
+            <div class="rounded bg-white w-11/12 sm:w-4/5 md:w-3/5 h-5/6 md:h-4/5 relative overflow-x-hidden overflow-y-auto">
+                <div class="h-20">
+                    <header class="rounded-t bg-white fixed w-11/12 sm:w-4/5 md:w-3/5
+                        flex px-6 py-4 margin-auto border-gray-300 border-b">
+                        <h1 class="flex-grow">{{$props.heading}}</h1>
+                        <button>
+                            <Icon id="close" />
+                        </button>
+                    </header>
+                </div>
+                <div class="px-6 pb-6 pb-12 rounded">
+                    <slot/>
+                </div>
+            </div>
+        </div>
+    </teleport>
+</template>
+
+<script>
+import Icon from '@/components/Icon.vue'
+
+export default {
+    components: {
+        Icon
+    },
+    props: {
+        heading: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
--- src/views/pages/Notifications.vue
+++ src/views/pages/Notifications.vue
@@ -1,7 +1,7 @@
 <template>
     <Page title="Notifications">
         <Section heading="Toasts" description="Click any of the buttons to trigger the toasts."
-            class="flex">
+            class="block md:flex">
             <div class="buttons-group flex flex-grow flex-wrap justify-center my-auto">
                 <Button type="primary" :action="()=>showToast()">Default</Button>
                 <Button type="secondary" :action="()=>showToast('success')">Success</Button>
@@ -11,7 +11,9 @@
             </div>
         </Section>
         <Section heading="Modals" description="Click any of the buttons to trigger the modals.">
-
+            <Modal heading="A Sample Modal">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed velit dignissim sodales ut eu sem integer vitae. Id aliquet lectus proin nibh nisl condimentum. Fringilla urna porttitor rhoncus dolor purus. Nam aliquam sem et tortor. Nisl vel pretium lectus quam id. Cras pulvinar mattis nunc sed. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat orci. Tristique magna sit amet purus. Fermentum et sollicitudin ac orci phasellus egestas tellus. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Felis eget nunc lobortis mattis aliquam faucibus. Tincidunt eget nullam non nisi est sit amet facilisis. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Eget nunc scelerisque viverra mauris in aliquam sem fringilla ut. Nec nam aliquam sem et tortor consequat id. Commodo nulla facilisi nullam vehicula ipsum a. Elementum tempus egestas sed sed. Faucibus purus in massa tempor nec feugiat nisl pretium fusce.
+            </Modal>
         </Section>
     </Page>
 </template>
@@ -20,11 +22,12 @@
 import Page from '@/components/Page.vue'
 import Section from '@/components/Section.vue'
 import Button from '@/components/Button.vue'
+import Modal from '@/components/Modal.vue'
 import {useToast} from 'vue-toastification'
 
 export default {
     components: {
-        Page, Section, Button
+        Page, Section, Button, Modal
     },
     setup(){
         const toast = useToast()
