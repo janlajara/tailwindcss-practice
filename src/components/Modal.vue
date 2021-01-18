0204829d282c0fc89b63c21d8a8a45d33e400547
Jan Lajara <janlajara@gmail.com>
2021-01-18 22:09:21 +0800

    fixed modal height issues

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,19 +1,19 @@
 <template>
     <teleport to="body">
         <div class="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-30">
-            <div class="rounded bg-white w-11/12 sm:w-4/5 md:w-3/5 h-5/6 md:h-4/5 relative overflow-x-hidden overflow-y-auto">
-                <div class="h-20">
-                    <header class="rounded-t bg-white fixed w-11/12 sm:w-4/5 md:w-3/5
-                        flex px-6 py-4 margin-auto border-gray-300 border-b">
-                        <h1 class="flex-grow">{{$props.heading}}</h1>
-                        <button>
-                            <Icon id="close" />
-                        </button>
-                    </header>
-                </div>
-                <div class="px-6 pb-6 pb-12 rounded">
+            <div class="modal relative ">
+                <header class="modal-header border-b">
+                    <h1 class="flex-grow">{{$props.heading}}</h1>
+                    <button>
+                        <Icon id="close" />
+                    </button>
+                </header>
+                <div class="modal-body pb-6 pb-12 h-96 overflow-y-auto">
                     <slot/>
                 </div>
+                <footer class="modal-footer border-t">
+                    Footer goes here.
+                </footer>
             </div>
         </div>
     </teleport>
@@ -34,3 +34,20 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+@layer components {
+    .modal {
+        @apply w-11/12 sm:w-4/5 md:w-3/5;
+        @apply rounded bg-white;
+    }
+    .modal-body, .modal-footer, .modal-header {
+       @apply w-full px-6;
+    }
+    .modal-footer, .modal-header {
+        @apply py-4;
+        @apply border-gray-300;
+        @apply flex my-auto;
+    }
+}
+</style>
