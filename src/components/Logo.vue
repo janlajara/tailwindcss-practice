7063b96e51308dc39057c034e05d3cc5f9cdde02
Jan Lajara <janlajara@gmail.com>
2021-02-17 15:13:28 +0800

    resize logo text

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex justify-left m-auto">
         <span class="material-icons text-gray-900 sm:text-primary w-10 text-center sm:text-3xl">tapas</span>
-        <span class="font-serif flex-grow text-2xl sm:text-3xl italic font-black tracking-tighter">Mmmsherep</span>
+        <span class="font-serif flex-grow text-2xl italic font-black tracking-tighter">Mmmsherep</span>
     </div>
 </template>
