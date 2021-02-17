3c9acb5c445149e444f492fad4a8967a21a2b50d
Jan Lajara <janlajara@gmail.com>
2021-02-17 14:44:01 +0800

    rename from Sherep to Mmmsherep

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="py-8 px-8 md:px-16 text-center md:text-left">
-        <p class="text-sm">Copyright © Sherep! 2021</p>
+    <footer class="py-8 px-8 md:px-16 text-center md:text-left bg-pattern-clouds">
+        <p class="text-sm">Copyright © Mmmsherep 2021</p>
     </footer>
 </template>
