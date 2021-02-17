bc7c8b2a0d7c36cef3d0180b2dc2a2d78a7aecd8
Jan Lajara <janlajara@gmail.com>
2021-02-17 16:44:53 +0800

    redirect to home on logo click

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex justify-left m-auto">
+    <div class="flex justify-left m-auto cursor-pointer" @click="$router.push({name: 'Home'})">
         <span class="material-icons text-gray-900 sm:text-primary w-10 text-center sm:text-3xl">tapas</span>
         <span class="font-serif flex-grow text-2xl italic font-black tracking-tighter">Mmmsherep</span>
     </div>
